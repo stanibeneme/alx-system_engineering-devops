599ec83f75f3b71bca7695022c00fe73d8ca9bc0
Stanley Ibeneme <stanleyibeneme92@gmail.com>
2024-01-13 13:42:51 -0800

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,5 @@
-# Install Flask v2.1.0 using pip3
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
-  require => Package['python3-pip'],
-}
 # Install flask v2.1.0 from pip3
-# package { 'flask':
-#  ensure   => '2.1.0',
-#  provider => 'pip3',
-#}
+ package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
